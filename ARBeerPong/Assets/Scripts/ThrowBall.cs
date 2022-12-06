21abfc0b00a7b2e3a1bf5b7d5225e43e9c95d650
AmalGeorgeJacob\Amal George Jacob <amaljacob05@gmail.com>
2022-12-06 20:52:37 +0000

    Ball And cup

---
+++ ARBeerPong/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject blubberBallPrefab;
+    [SerializeField] private float throwForce = 2.0f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Debug.Log(Input.mousePosition);
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 worldMousePos = mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, mainCamera.nearClipPlane));
+            Debug.Log("World Mouse Position: " + worldMousePos);
+            Vector3 directionVector = worldMousePos - mainCamera.transform.position;
+            directionVector = new Vector3(directionVector.x, directionVector.y, 0f).normalized;
+            Debug.Log("Direction Vector: " + directionVector);
+            Vector3 forceVector = new Vector3(directionVector.x, directionVector.y, 1.0f * throwForce);
+
+            GameObject blubberBall = Instantiate(blubberBallPrefab, mainCamera.transform.position, Quaternion.identity);
+            Rigidbody rigidBlubber = blubberBall.GetComponent<Rigidbody>();
+            rigidBlubber.AddForce(forceVector, ForceMode.Impulse);
+        }
+    }
+
+}
