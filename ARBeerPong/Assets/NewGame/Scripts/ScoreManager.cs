ad22197de275866ef5855f99c46d052125303877
nikunj2706 <nikunjsth5@gmail.com>
2022-12-12 14:28:40 +0000

    scoring update

--- ARBeerPong/Assets/NewGame/Scripts/ScoreManager.cs
+++ ARBeerPong/Assets/NewGame/Scripts/ScoreManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class ScoreManager : MonoBehaviour
 {
+	static float scoreData;
+	[SerializeField] TextMeshProUGUI scoreUI;
 	void OnTriggerEnter(Collider other)
 	{
 
 		// Nikunj Add Score Here
 		FindObjectOfType<AudioManager>().Play("HitInside");
-
+		Destroy(this.transform.parent.gameObject);
+		scoreData++;
+		scoreUI.text = scoreData.ToString();
 
 	}
 }
