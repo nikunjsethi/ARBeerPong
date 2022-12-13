49adbdd81a5c109a35cd49d08bbbcd59e83d975c
nikunj2706 <nikunjsth5@gmail.com>
2022-12-13 11:29:21 +0000

    UI modifications, win/lose screen added

--- ARBeerPong/Assets/NewGame/Scripts/ScoreManager.cs
+++ ARBeerPong/Assets/NewGame/Scripts/ScoreManager.cs
@@ -7,14 +7,51 @@ public class ScoreManager : MonoBehaviour
 {
 	public BallManager _ballManager;
 	static float scoreData;
+	static float cupsDown;
 	[SerializeField] TextMeshProUGUI scoreUI;
-	void OnTriggerEnter(Collider other)
+	[SerializeField] GameObject mainCanvas;
+	[SerializeField] GameObject winCanvas;
+	[SerializeField] GameObject loseCanvas;
+	[SerializeField] GameObject[] arCamerasToDisable;
+	[SerializeField] GameObject uiCamera;
+
+    private void Start()
+    {
+		scoreData = 0;
+		scoreUI.text = "Score : " + scoreData.ToString();
+	}
+    void OnTriggerEnter(Collider other)
 	{
 		// Nikunj Add Score Here
 		FindObjectOfType<AudioManager>().Play("HitInside");
 		Destroy(this.transform.parent.gameObject,1.5f);
 		scoreData++;
+		cupsDown++;
 		scoreUI.text = "Score : "+scoreData.ToString();
 		Destroy(_ballManager._newBall.gameObject);
+
+		if(cupsDown>=2)
+        {
+            StartCoroutine(ChangeUI());
+            //for (int i = 0; i < arCamerasToDisable.Length; i++)
+            //    arCamerasToDisable[i].SetActive(false);
+            //mainCanvas.SetActive(false);
+            //uiCamera.SetActive(true);
+            //winCanvas.SetActive(true);
+            //cupsDown = 0;
+            //scoreData = 0;
+        }
+	}
+
+	IEnumerator ChangeUI()
+    {
+		yield return new WaitForSeconds(0f);
+		for (int i = 0; i < arCamerasToDisable.Length; i++)
+			arCamerasToDisable[i].SetActive(false);
+		mainCanvas.SetActive(false);
+		uiCamera.SetActive(true);
+		winCanvas.SetActive(true);
+		cupsDown = 0;
+		scoreData = 0;
 	}
 }
