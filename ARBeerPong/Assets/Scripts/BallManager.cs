a237d1351587991fecb8418badb98d5cbc64a239
AmalGeorgeJacob\Amal George Jacob <amaljacob05@gmail.com>
2022-12-15 04:48:31 +0000

    UI, Plane, Cups

--- ARBeerPong/Assets/Scripts/BallManager.cs
+++ ARBeerPong/Assets/Scripts/BallManager.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
     [SerializeField] private GameObject _ball;
-    public GameObject _newBall;
+    [SerializeField] private GameObject _cups;
+    [HideInInspector] public GameObject _newBall;
+    private float _distance;
+
     public void SpawnBall()
     {
         Invoke("CreateBall", 2f);
@@ -13,7 +15,7 @@ public class BallManager : MonoBehaviour
     private void CreateBall()
     {
         _newBall = Instantiate(_ball, this.transform.position, Quaternion.identity);
-        _newBall.transform.parent = this.transform;  
+        _newBall.transform.parent = this.transform;
         _newBall.SetActive(true);
 
     }
@@ -21,6 +23,26 @@ public class BallManager : MonoBehaviour
     public void UnparentMe()
     {
         _newBall.transform.parent = null; // need to remove parent after ball is thrown so it doesnt move with the camera
+    }
+    private void Update()
+    {
+        if (transform.childCount > 0)
+        {
+            _distance = Vector3.Distance(this.transform.position, _cups.transform.position);
+
+            if (_distance < 1f)
+            {
+                if (gameObject.transform.GetChild(0).gameObject.activeInHierarchy)
+                    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            else
+            {
+
+                if (!gameObject.transform.GetChild(0).gameObject.activeInHierarchy)
+                    gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+
     }
 
 }
